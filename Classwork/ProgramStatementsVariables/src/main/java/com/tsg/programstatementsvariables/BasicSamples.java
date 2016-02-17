489025a2dc74356f568fe27af7af2b8a21469170
Levi Schneider <levi.schneider2010@gmail.com>
2016-02-17 10:42:43 -0500

    LTSSW-3 adding windowmaster file

--- Classwork/ProgramStatementsVariables/src/main/java/com/tsg/programstatementsvariables/BasicSamples.java
+++ Classwork/ProgramStatementsVariables/src/main/java/com/tsg/programstatementsvariables/BasicSamples.java
@@ -5,7 +5,6 @@ package com.tsg.programstatementsvariables;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.tsg.programstatementsvariables;
 /**
  *
  * @author apprentice
---
+++ PBD/PBD3.java
@@ -0,0 +1,8 @@
+public class PBD3
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Mr. Mitchell is cool.");
+	}
+}
+
