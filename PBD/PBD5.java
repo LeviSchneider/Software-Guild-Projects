b2679063dd524da2ea94e4c52d128e085087a796
Levi Schneider <levi.schneider2010@gmail.com>
2016-02-17 10:59:35 -0500

    LTSSW-6 This is the PBD5 file!

---
+++ PBD/PBD5.java
@@ -0,0 +1,15 @@
+public class PBD5 
+{
+	public static void main(String[] args)
+	{
+		//a comment, this is so you can read your program later.
+		//anything after the // is ignored by Java.
+
+		System.out.println("I could have code like this."); //and the comment after it ignored.
+
+		//You can also use a comment to "disable" or comment out a piece of code:
+		//System.out.println("This won't run.");
+
+		System.out.println("This will run.");
+	}
+}
