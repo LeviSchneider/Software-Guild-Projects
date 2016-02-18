59b8558bd757b9ab2275df4192c78cbb73916036
Levi Schneider <levi.schneider2010@gmail.com>
2016-02-17 22:29:12 -0500

    Programming By Doing exercise

---
+++ PBD/PBD7.java
@@ -0,0 +1,12 @@
+public class PBD7 {
+	public static void main(String[] args){
+
+		System.out.println("L      TTTTTTT   SSS  ");
+		System.out.println("L         T     S   S ");
+		System.out.println("L         T     S     ");
+		System.out.println("L         T      SSS  ");
+		System.out.println("L         T         S ");
+		System.out.println("L         T     S   S ");
+		System.out.println("LLLLL     T      SSS  ");
+	}
+}
