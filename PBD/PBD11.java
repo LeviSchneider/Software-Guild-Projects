7b9cad1e5c035004230c1d9775abb43bcfe4900a
Levi Schneider <levi.schneider2010@gmail.com>
2016-02-18 19:16:13 -0500

    LTSSW-10 adding PBD11 files

---
+++ PBD/PBD11.java
@@ -0,0 +1,13 @@
+public class PBD11{
+	public static void main(String[] args){
+		int num;
+		double irNum;
+		String computer;
+		num = 113;
+		irNum = 2.71828f;
+		computer = "Computer Science";
+		System.out.println( "This is room # " + num );
+    	System.out.println( "e is close to " + irNum );
+    	System.out.println( "I am learning a bit about " + computer );
+	}
+}
