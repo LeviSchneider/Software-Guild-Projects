83883ec0131e71876ab0fcf32985738a96fe5efa
Levi Schneider <levi.schneider2010@gmail.com>
2016-02-18 19:21:21 -0500

    LTSSW-11 adding PBD12 files into bitbucket

---
+++ PBD/PBD12.java
@@ -0,0 +1,9 @@
+public class PBD12 {
+	public static void main(String[] args){
+		String name;
+		int num;
+		name = "Juan Valdez";
+		num = 2010;
+		System.out.println("My name is " + name + " and I'll graduate in " + num + ".");
+	}
+}
