26b01798e93908b2746928f4c1981f8ea87b9588
Levi Schneider <levi.schneider2010@gmail.com>
2016-02-22 14:38:05 -0500

    JavaBasics Counter

---
+++ JavaBasics1/src/main/java/com/tsg/javabasics/Counter.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tsg.javabasics;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author apprentice
+ */
+public class Counter {
+    public static void main(String[] args) {
+        System.out.println(to10());
+    }
+    public static int to10()
+    {
+        int number = 0;
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.println(i);
+            number = i;
+        }  
+        return number;
+    }
+    public static int toN(int choice)
+    {
+        int number = 0;
+        for(int i = 1; i < choice; i++)
+        {
+            System.out.println(i);
+        }
+        return number;
+    }
+}
