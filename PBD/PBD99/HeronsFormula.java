54e71a375670b566e4b7bd551fc016246c4a9278
Levi Schneider <levi.schneider2010@gmail.com>
2016-02-22 21:28:29 -0500

    PBD99 files

---
+++ PBD/PBD99/HeronsFormula.java
@@ -0,0 +1,35 @@
+public class HeronsFormula
+{
+//Problem 1) No difference between the two files in terms of output.
+//Problem 2) HeronsFormulaNoFunction.java has 50 lines of code while HeronsFormula.java has 32.
+//Problem 3) 
+	public static void main( String[] args )
+	{
+		double a;
+		
+		a = triangleArea(3, 3, 3);
+		System.out.println("A triangle with sides 3,3,3 has an area of " + a );
+
+		a = triangleArea(3, 4, 5);
+		System.out.println("A triangle with sides 3,4,5 has an area of " + a );
+ 
+		a = triangleArea(7, 8, 9);
+		System.out.println("A triangle with sides 7,8,9 has an area of " + a );
+
+		System.out.println("A triangle with sides 5,12,13 has an area of " + triangleArea(5, 12, 13) );
+		System.out.println("A triangle with sides 10,9,11 has an area of " + triangleArea(10, 9, 11) );
+		System.out.println("A triangle with sides 8,15,17 has an area of " + triangleArea(8, 15, 17) );
+	}
+ 
+	public static double triangleArea( float a, float b, float c )
+	{
+		// the code in this function computes the area of a triangle whose sides have lengths a, b, and c
+		double s, A;
+
+		s = (a+b+c) / 2.0;
+		A = Math.sqrt( s*(s-a)*(s-b)*(s-c) );
+
+		return A;
+		// ^ after computing the area, "return" it
+	}
+}
