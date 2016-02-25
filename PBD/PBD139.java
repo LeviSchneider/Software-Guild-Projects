4ded75408ab152e9b68f07a8168c88272969be78
Levi Schneider <levi.schneider2010@gmail.com>
2016-02-24 21:27:53 -0500

    LTSSW-7 PBD138/139

---
+++ PBD/PBD139.java
@@ -0,0 +1,12 @@
+public class PBD139
+{
+	public static void main(String[] args)
+	{
+		int[] array = {-113, -113, -113, -113, -113, -113, -113, -113, -113, -113};
+
+		for(int i = 0; i < array.length; i++)
+		{
+			System.out.println("Slot " + i + " contains a " + array[1]);
+		}
+	}
+}
